5bb0a68e699ae10921e77e02bf13445f8dd22cb3
Saloni-Mehtre <salonimehtre002@gmail.com>
2025-08-29 19:07:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_string = ''
 
 ingredients_list = st.multiselect(
-    'Choose up to 5 ingredients:',
+    'Choose up to 5 ingredients:'),
      my_dataframe,
     max_selections= 5
 )
