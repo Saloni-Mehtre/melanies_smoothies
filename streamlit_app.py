1e088c01683adbbaa95c54097e5e27996f16362a
Saloni-Mehtre <salonimehtre002@gmail.com>
2025-08-31 21:29:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,9 +29,12 @@ st.write("The name on the smoothie will be", name_on_order)
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+#Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function 
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
-
 # Define ingredients_string before the if block
 
 ingredients_string = ''
